3e5769a4bef2aefb6e2fbbfa29a4c782de32d94c
Mohammed Parves <mdperrfan@gmail.com>
2024-03-14 15:38:17 +0600

    Updated Styling for Create Join class

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php 
 ob_start(); 
-session_start();
+@session_start();
 $timezone = date_default_timezone_set("Asia/Dhaka");
 $con = mysqli_connect("localhost", "root", "", "virtualclassroom");
 if(mysqli_connect_errno())
--- createJoinClass.php
+++ createJoinClass.php
@@ -6,6 +6,7 @@ require 'handler/createJoinClasshandler.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,61 +15,67 @@ require 'handler/createJoinClasshandler.php';
     <link rel="stylesheet" href="cj.css">
 </head>
 <body>
-<div class="bg">
-    <div class="wrapper">
-        <div class="creatClass_box">  	
-            <div id="first">
-                <div class="creatClass_header">
-                    <h1>Create Class</h1>
-                </div>
-                <form action="createJoinClass.php" method="POST">
-                    <input type="text" name="className" autocomplete="off" placeholder="Class Name/Course Code" value="">
-                    <br>
-                    <input type="text" name="section" autocomplete="off" placeholder="Section" value="">
-                    <br>
-                    <input type="text" name="subject" autocomplete="off" placeholder="Subject/Course Title" value="">
-                    <br>
-                    <button class="cancel_button"><a href="index.php">Cancel</a></button>
-                    <button type="submit" name="createClass_button" id="create_class_button">Create</button>
-                    <br>
-                    <br>
-                    <a href="#" id="joinClass" class="joinClass">Want to join a Class? Click Here</a>
-                </form>
-            </div>
-             
-            <div id="second">
-                <div class="joinClass_header">
-                    <h1>Join class</h1>
-                </div>
-                <form action="createJoinClass.php" method="POST">
-                    <input type="text" name="code" placeholder="Class code" autocomplete="off" value="<?php 
-                        if(isset($_SESSION['code'])){
-                            echo $_SESSION['code'];
-                        } 
-                    ?>">
-                    <br>
-                    <button class="cancel_button"><a href="index.php">Cancel</a></button>
-                    <button type="submit" name="joinClass_button" id="join_class_button">Join</button>
-                    <br>
-                    <br>
-                    <a href="#" id="createClass" class="createClass">Want to create a new Class? Click here!</a>
-                </form>
-            </div>
-        </div>          
+
+                <div id="first" class="subscribe-box">
+    <div class="creatClass_header">
+        <h1>Create Class</h1>
     </div>
+    <form class="subscribe" action="createJoinClass.php" method="POST">
+        <div class="form-input-material">
+            <input type="text" name="className" autocomplete="off" placeholder="Class Name/Course Code" class="form-control-material" value="">
+            <label for="className"></label>
+        </div>
+        <div class="form-input-material">
+            <input type="text" name="section" autocomplete="off" placeholder="Section" class="form-control-material" value="">
+            <label for="section"></label>
+        </div>
+        <div class="form-input-material">
+            <input type="text" name="subject" autocomplete="off" placeholder="Subject/Course Title" class="form-control-material" value="">
+            <label for="subject"></label>
+        </div>
+        <button type="submit" name="createClass_button" id="create_class_button" class="subscribe-button btn btn-primary btn-ghost">Create</button>
+        <br>
+        <br>
+        <a href="#" id="joinClass" class="joinClass">Want to join a Class? Click Here</a>
+    </form>
 </div>
-               
+
+
+
+                <div id="second" class="subscribe-box">
+                    <div class="joinClass_header">
+                        <h1>Join class</h1>
+                    </div>
+                     <form class="subscribe" action="createJoinClass.php" method="POST">
+                     <input type="text" name="code" placeholder="Class code" autocomplete="off" value="<?php 
+                                                         if(isset($_SESSION['code'])){
+                                                         echo $_SESSION['code'];
+                                                                                } 
+                                                                            ?>">
+                        <br>
+                     <button type="submit" name="joinClass_button" id="join_class_button" class="subscribe-button">Join</button>
+                     <br>
+                      <br>
+                      <a href="#" id="createClass" class="createClass">Want to create a new Class? Click here!</a>
+                    </form>
+                </div>
+
 </body>
+
 </html>
 
 <script>
     // jQuery script to show/hide sections
-    $(document).ready(function(){
+    $(document).ready(function() {
         $("#first").hide(); // Initially hide the create class section
         $("#second").show(); // Show the join class section
 
         // Toggle between Create Class and Join Class sections
-        $("#createClass").click(function(){
+        $("#createClass").click(function() {
+            $("#first").toggle();
+            $("#second").toggle();
+        });
+        $("#joinClass").click(function() {
             $("#first").toggle();
             $("#second").toggle();
         });
